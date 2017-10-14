02714e30026f0218d6521977510a31641e21dd62
Selim Chiki <selimchiki@yahoo.fr>
2017-10-14 18:49:40 +0200

    Création du controller Sessions pour la connexion des utilisateurs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/home'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
