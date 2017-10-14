bc6cb470ba110450a7010f3ada58f60f0d4741d6
Selim Chiki <selimchiki@yahoo.fr>
2017-10-14 21:52:55 +0200

    Création de la page d'edition de l'utilisateur
    Pour modifier son mot de passe et son email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,11 +19,30 @@ class SessionsController < ApplicationController
   end
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy
     log_out
     redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
   get '/users/:id', to: "sessions#show", as: "user"
+  get '/users/:id/edit', to: "sessions#edit", as: "edit_user"
+  patch '/users/:id', to: "sessions#update"
   delete 'sessions/destroy', to: "sessions#destroy", as: "logout"
 
   devise_for :admin_users, ActiveAdmin::Devise.config
