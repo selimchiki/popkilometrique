1644e8adb13c698f13774035dd334274fe655a39
Selim Chiki <selimchiki@yahoo.fr>
2018-02-08 14:54:18 +0100

    Add Rspec Model Car & Fee

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -5,8 +5,22 @@ RSpec.describe Car, type: :model do
     it 'should create an Car' do
       user = create(:user)
       scale = create(:scale)
-      create(:car, user: user, scale: scale)
+      create(:car, model: "C4", user: user, scale: scale)
       expect(Car.count).to eq(1)
+    end
+
+    it 'should not create an Car for empty model' do
+      car = build(:car_not_model)
+      car.validate
+      car.save
+      expect(Car.count).to eq(0)
+    end
+
+    it 'should not create an Car for empty scale' do
+      car = build(:car_not_scale)
+      car.validate
+      car.save
+      expect(Car.count).to eq(0)
     end
 
 end
