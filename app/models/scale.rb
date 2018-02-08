266744d4adc995888bb9ad6e41be336c654e2e8d
Selim Chiki <selimchiki@yahoo.fr>
2018-02-08 16:01:49 +0100

    Modify Scale Model & Add Rspec Model Scale

--- app/models/scale.rb
+++ app/models/scale.rb
@@ -2,6 +2,11 @@ class Scale < ApplicationRecord
 
   has_many :cars
 
+  validates :vehicle_type, presence: true
+  validates :fiscal_power, presence: true
+  validates :coefficient, presence: true
+  validates :active, inclusion: { in: [true, false] }
+
   def model_with_fiscal
     "#{vehicle_type}, #{fiscal_power}"
   end
--- spec/factories/scales.rb
+++ spec/factories/scales.rb
@@ -7,4 +7,33 @@ FactoryBot.define do
     active true
   end
 
+  factory :scale_not_vehicle_type, class: Scale do
+    vehicle_type nil
+    fiscal_power "2CV"
+    coefficient 0.31
+    active true
+  end
+
+  factory :scale_not_fiscal_power, class: Scale do
+    vehicle_type "Vehicule Leger"
+    fiscal_power nil
+    coefficient 0.31
+    active true
+  end
+
+  factory :scale_not_coefficient, class: Scale do
+    vehicle_type "Vehicule Leger"
+    fiscal_power "2CV"
+    coefficient nil
+    active true
+  end
+
+
+  factory :scale_not_active, class: Scale do
+    vehicle_type "Vehicule Leger"
+    fiscal_power "2CV"
+    coefficient 0.31
+    active nil
+  end
+
 end
