c02e8efd5081fb098924ef984db0ae499c452fa0
Selim Chiki <selimchiki@yahoo.fr>
2018-02-28 15:22:05 +0100

    add rspec controller dor SessionController

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -14,12 +14,35 @@ RSpec.describe SessionsController, type: :controller do
     context "user not signed" do
       it "redirect_to root_path" do
         get :home
-        expect(response).to redirect_to root_path
+        expect(response.status).to eq(200)
       end
     end
   end
 
   describe "GET :show" do
 
+    log_in_user
+
+    it "assign the requested user to @user" do
+      get :show, params: { id: @user }
+      expect(assigns(:user)).to eq(@user)
+    end
+
+    it "assign the requested car to @car" do
+      @scale = create(:scale)
+      @car = create(:car, user_id: @user.id, scale_id: @scale.id)
+      @car_user = @user.cars
+      get :show, params: { id: @user }
+      expect(assigns(:cars)).to eq(@car_user)
+    end
+
+    it "assign the requested fees to @fee" do
+      @scale = create(:scale)
+      @car = create(:car, user_id: @user.id, scale_id: @scale.id)
+      @fee = create(:fee, user_id: @user.id, car: @car.id)
+      @user_fee = @user.fees
+      get :show, params: { id: @user }
+      expect(assigns(:fees)).to eq(@user_fee)
+    end
   end
 end
